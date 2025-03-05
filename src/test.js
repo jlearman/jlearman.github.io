b54c5cb8ead35ac5be380e7e90d401e560108350
Jeff Learman <jjlearman@gmail.com>
2025-03-05 10:40:36 -0500

    test.js - example use of stringsync/musicxml

--- src/test.js
+++ src/test.js
@@ -1,16 +1,121 @@
-import { asserts, elements, MusicXML } from '@stringsync/musicxml';
+// example from https://github.com/stringsync/musicxml/issues/5
 
-function show_score() {
-    const score = MusicXML.createPartwise();
-    const root = score.getRoot();
-    console.log(score.serialize())
+import { elements, MusicXML } from '@stringsync/musicxml';
 
-    const note = new elements.Note // how to associate the pitch? how to add to measure?
-    const measure = new elements.MeasurePartwise({ attributes: { number: '1', implicit: 'no' } });
-    const part = new elements.PartPartwise([measure])
-    root.setParts([part])
-    console.log(score.serialize()) // output same as above
+const musicXML = MusicXML.createPartwise();
 
-}
+musicXML
+  .getRoot()
+  .setPartList(
+    new elements.PartList({
+      contents: [
+        new Array(),
+        new elements.ScorePart({
+          attributes: { id: 'P1' },
+          contents: [
+            null, // elements.Identification
+            new Array(),
+            new elements.PartName({ contents: ['Part 1'] }),
+            null, // elements.PartNameDisplay
+            null, // elements.PartAbbreviation
+            null, // elements.PartAbbreviationDisplay
+            new Array(),
+            new Array(),
+            new Array(),
+            new Array(),
+          ],
+        }),
+        new Array(),
+      ],
+    })
+  )
+  .setParts([
+    new elements.PartPartwise({
+      attributes: { id: 'P1' },
+    }).setMeasures([
+      new elements.MeasurePartwise({
+        attributes: { number: '1' },
+        contents: [
+          [
+            new elements.Attributes({
+              attributes: { divisions: 1 },
+              contents: [
+                null, // elements.Footnote
+                null, // elements.Level
+                null, // elements.Divisions
+                new Array(),
+                [
+                  new elements.Time({
+                    contents: [
+                      [
+                        [
+                          [
+                            new elements.Beats({
+                              contents: ['4'],
+                            }),
+                            new elements.BeatType({
+                              contents: ['4'],
+                            }),
+                          ],
+                        ],
+                        null,
+                      ],
+                    ],
+                  })
+                ],
+                null, // elements.Staves
+                null, // elements.PartSymbol
+                null, // elements.Instruments
+                new Array(),
+                new Array(),
+                new Array(),
+                new Array(),
+                new Array(),
+              ],
+            }),
+            new elements.Note({
+              contents: [
+                [
+                  null, // elements.TiedNote
+                  new elements.Pitch({
+                    contents: [
+                      new elements.Step({
+                        contents: ['C'],
+                      }),
+                      null, // elements.Alter
+                      new elements.Octave({
+                        contents: [4],
+                      }),
+                    ],
+                  }),
+                  new elements.Duration({
+                    contents: [4],
+                  }),
+                  [], // elements.Tie,
+                ],
+                new Array(),
+                null, // elements.Footnote
+                null, // elements.Level
+                null, // elements.Voice
+                null, // elements.Type
+                new Array(),
+                null, // elements.Accidental
+                null, // elements.TimeModification
+                null, // elements.Stem
+                null, // elements.Notehead
+                null, // elements.NoteheadText
+                null, // elements.Staff
+                [], // elements.Beam
+                new Array(),
+                new Array(),
+                null, // elements.Play
+                null, // elements.Listen
+              ],
+            }),
+          ],
+        ],
+      }),
+    ]),
+  ]);
 
-show_score();
+console.log(musicXML.serialize());
--- src/test.ts
+++ src/test.ts
@@ -1,15 +1,117 @@
-import { asserts, elements, MusicXML } from '@stringsync/musicxml';
+const musicXml = MusicXML.createPartwise();
 
-function show_score() {
-    const score = MusicXML.createPartwise();
-    const root = score.getRoot();
-    console.log(score.serialize())
+musicXml
+.getRoot()
+.setPartList(
+    new elements.PartList({
+    contents: [
+        new Array<elements.PartGroup>(),
+        new elements.ScorePart({
+        attributes: { id: 'P1' },
+        contents: [
+            null, // elements.Identification
+            new Array<elements.PartLink>(),
+            new elements.PartName({ contents: ['Part 1'] }),
+            null, // elements.PartNameDisplay
+            null, // elements.PartAbbreviation
+            null, // elements.PartAbbreviationDisplay
+            new Array<elements.Group>(),
+            new Array<elements.ScoreInstrument>(),
+            new Array<elements.Player>(),
+            new Array<elements.MidiDevice | elements.MidiInstrument>(),
+        ],
+        }),
+        new Array<elements.PartGroup | elements.ScorePart>(),
+    ],
+    })
+)
+.setParts([
+    new elements.PartPartwise({
+    attributes: { id: 'P1' },
+    }).setMeasures([
+    new elements.MeasurePartwise({
+        attributes: { number: '1' },
+        contents: [
+        [
+            new elements.Attributes({
+            attributes: { divisions: 1 },
+            contents: [
+                null, // elements.Footnote
+                null, // elements.Level
+                null, // elements.Divisions
+                new Array<elements.Key>(),
+                new Array<elements.Time>(
+                new elements.Time({
+                    contents: [
+                    [
+                        [
+                        [
+                            new elements.Beats({
+                            contents: ['4'],
+                            }),
+                            new elements.BeatType({
+                            contents: ['4'],
+                            }),
+                        ],
+                        ],
+                        null,
+                    ],
+                    ],
+                })
+                ),
+                null, // elements.Staves
+                null, // elements.PartSymbol
+                null, // elements.Instruments
+                new Array<elements.Clef>(),
+                new Array<elements.StaffDetails>(),
+                new Array<elements.Transpose>(),
+                new Array<elements.Directive>(),
+                new Array<elements.MeasureStyle>(),
+            ],
+            }),
+            new elements.Note({
+            contents: [
+                [
+                null, // elements.TiedNote
+                new elements.Pitch({
+                    contents: [
+                    new elements.Step({
+                        contents: ['C'],
+                    }),
+                    null, // elements.Alter
+                    new elements.Octave({
+                        contents: [4],
+                    }),
+                    ],
+                }),
+                new elements.Duration({
+                    contents: [4],
+                }),
+                [], // elements.Tie,
+                ],
+                new Array<elements.Instrument>(),
+                null, // elements.Footnote
+                null, // elements.Level
+                null, // elements.Voice
+                null, // elements.Type
+                new Array<elements.Dot>(),
+                null, // elements.Accidental
+                null, // elements.TimeModification
+                null, // elements.Stem
+                null, // elements.Notehead
+                null, // elements.NoteheadText
+                null, // elements.Staff
+                [], // elements.Beam
+                new Array<elements.Notations>(),
+                new Array<elements.Lyric>(),
+                null, // elements.Play
+                null, // elements.Listen
+            ],
+            }),
+        ],
+        ],
+    }),
+    ]),
+]);
 
-    const note = new elements.Note // how to associate the pitch? how to add to measure?
-    const measure = new elements.MeasurePartwise({ attributes: { number: '1', implicit: 'no' } });
-    const part = new elements.PartPartwise([measure])
-    root.setParts([part])
-    console.log(score.serialize()) // output same as above
-}
-
-show_score();
+console.log(nusicxml.serialize());
