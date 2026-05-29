d12ee54651ef1f6041e079f636f9c456bd7ce8be
Jeff Learman <jjlearman@gmail.com>
2026-05-29 16:42:22 -0400

    stable IDs, 7b5, 9s11
    Use stable IDs in xml and omit dates from index files, to minimize github churn when files are regenerated.

---
+++ src/score.js
@@ -0,0 +1,129 @@
+// example from https://github.com/stringsync/musicxml/issues/5
+
+import { elements, MusicXML } from '@stringsync/musicxml';
+
+const score = MusicXML.createPartwise();
+
+score
+  .getRoot()
+  .setPartList(
+    new elements.PartList({
+      contents: [
+        new Array(),
+        new elements.ScorePart({
+          attributes: { id: 'P1' },
+          contents: [
+            null, // elements.Identification
+            new Array(),
+            new elements.PartName({ contents: ['Part 1'] }),
+            null, // elements.PartNameDisplay
+            null, // elements.PartAbbreviation
+            null, // elements.PartAbbreviationDisplay
+            new Array(),
+            new Array(),
+            new Array(),
+            new Array(),
+          ],
+        }),
+        new Array(),
+      ],
+    })
+  );
+
+const measureAttrs = new elements.Attributes({
+  attributes: { divisions: 1 },
+  contents: [
+    null, // elements.Footnote
+    null, // elements.Level
+    null, // elements.Divisions
+    new Array(),
+    [
+      new elements.Time({
+        contents: [
+          [
+            [
+              [
+                new elements.Beats({
+                  contents: ['4'],
+                }),
+                new elements.BeatType({
+                  contents: ['4'],
+                }),
+              ],
+            ],
+            null,
+          ],
+        ],
+      })
+    ],
+    null, // elements.Staves
+    null, // elements.PartSymbol
+    null, // elements.Instruments
+    new Array(),
+    new Array(),
+    new Array(),
+    new Array(),
+    new Array(),
+  ],
+});
+
+const note1 = new elements.Note({
+  contents: [
+    [
+      null, // elements.TiedNote
+      new elements.Pitch({
+        contents: [
+          new elements.Step({
+            contents: ['C'],
+          }),
+          null, // elements.Alter
+          new elements.Octave({
+            contents: [4],
+          }),
+        ],
+      }),
+      new elements.Duration({
+        contents: [4],
+      }),
+      [], // elements.Tie,
+    ],
+    new Array(),
+    null, // elements.Footnote
+    null, // elements.Level
+    null, // elements.Voice
+    null, // elements.Type
+    new Array(),
+    null, // elements.Accidental
+    null, // elements.TimeModification
+    null, // elements.Stem
+    null, // elements.Notehead
+    null, // elements.NoteheadText
+    null, // elements.Staff
+    [], // elements.Beam
+    new Array(),
+    new Array(),
+    null, // elements.Play
+    null, // elements.Listen
+  ],
+
+})
+
+const measure = new elements.MeasurePartwise({
+  attributes: { number: '1' },
+  contents: [
+    [
+      measureAttrs,
+      note1,
+    ],
+  ],
+})
+
+score.getRoot().setParts([
+    new elements.PartPartwise({
+      attributes: { id: 'P1' },
+    }).setMeasures([
+      measure,
+    ]),
+  ]);
+
+console.log(score.serialize());
